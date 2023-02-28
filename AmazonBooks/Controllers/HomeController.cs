7f0d8fa6e455a2549395955531e8acbd3199451b
JSlaugh <93290565+JSlaugh@users.noreply.github.com>
2023-02-27 18:11:48 -0700

    Add project files.

---
+++ AmazonBooks/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using AmazonBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonBooks.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+        // This controllers main purpose is for displaying the home page which shows all of the books
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult Index() => View();
+    }
+}
